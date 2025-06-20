ec820e1a44b39708ea0f232ac8c5d765efab4cd8
armd482 <armd482@naver.com>
2025-06-20 16:09:57 +0900

    refactor: currentDate 밖에서 관리, 초기 스크롤 조건 추가

--- app/(main)/calendarPopover.tsx
+++ app/(main)/calendarPopover.tsx
@@ -1,5 +1,5 @@
 import { useDateStore } from '@/store/appointmentStore';
-import { useCallback, useState } from 'react';
+import { SetStateAction, useCallback, useState } from 'react';
 import { View } from 'react-native';
 import MonthPicker from './monthPicker';
 
@@ -9,15 +9,27 @@ interface CalendarPopoeverProps {
 
 export default function CalendarPopoever({ onClose }: CalendarPopoeverProps) {
   const date = useDateStore(state => state.date);
+  const [currentDate, setCurrentDate] = useState<Date>(date);
   const [type, setType] = useState<'month' | 'year'>('month');
 
   const handleChangeType = useCallback((value: 'month' | 'year') => {
     setType(value);
   }, []);
 
+  const handleChangeCurrentDate = useCallback((value: SetStateAction<Date>) => {
+    setCurrentDate(value);
+  }, []);
+
   return (
     <View className="flex size-full flex-col">
-      <MonthPicker startYear={1900} endYear={2100} value={date} onChangeType={handleChangeType} onClose={onClose} />
+      <MonthPicker
+        startYear={1900}
+        endYear={2100}
+        currentDate={currentDate}
+        onChangeCurrentDate={handleChangeCurrentDate}
+        onChangeType={handleChangeType}
+        onClose={onClose}
+      />
     </View>
   );
 }
--- app/(main)/monthPicker.tsx
+++ app/(main)/monthPicker.tsx
@@ -1,12 +1,14 @@
 import { useDateStore } from '@/store/appointmentStore';
 import { isSameMonth } from '@/utils/date';
-import { memo, useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { Dispatch, memo, SetStateAction, useCallback, useEffect, useMemo, useRef } from 'react';
 import { FlatList, Pressable, Text, View, ViewToken } from 'react-native';
+import { useShallow } from 'zustand/react/shallow';
 
 interface MonthPickerProps {
   startYear: number;
   endYear: number;
-  value: Date;
+  currentDate: Date;
+  onChangeCurrentDate: Dispatch<SetStateAction<Date>>;
   onChangeType: (value: 'month' | 'year') => void;
   onClose: () => void;
 }
@@ -14,18 +16,18 @@ interface MonthPickerProps {
 interface MonthSectionRowProps {
   year: number;
   monthSection: number;
-  value: Date;
   currentYear: number;
   onPressMonth: (year: number, month: number) => void;
 }
 
-const MonthSectionRow = memo(({ year, monthSection, value, currentYear, onPressMonth }: MonthSectionRowProps) => {
+const MonthSectionRow = memo(({ year, monthSection, currentYear, onPressMonth }: MonthSectionRowProps) => {
+  const date = useDateStore(state => state.date);
   return (
     <View className="flex h-[50px] w-full items-center justify-between gap-2 p-1">
       {Array.from({ length: 4 }).map((_, i) => (
         <Pressable
           key={i}
-          className={`aspect-square flex-1 rounded-full active:bg-gray-100 ${isSameMonth(value, new Date(year, monthSection * 4 + i)) ? 'bg-blue-100' : 'bg-white'}`}
+          className={`aspect-square flex-1 rounded-full active:bg-gray-100 ${isSameMonth(date, new Date(year, monthSection * 4 + i)) ? 'bg-blue-100' : 'bg-white'}`}
           onPress={() => onPressMonth(year, monthSection * 4 + i)}
         >
           <View className="flex size-full items-center justify-center">
@@ -37,12 +39,23 @@ const MonthSectionRow = memo(({ year, monthSection, value, currentYear, onPressM
   );
 });
 
-export default function MonthPicker({ startYear, endYear, value, onChangeType, onClose }: MonthPickerProps) {
+export default function MonthPicker({
+  startYear,
+  endYear,
+  currentDate,
+  onChangeCurrentDate,
+  onChangeType,
+  onClose,
+}: MonthPickerProps) {
   const listRef = useRef<FlatList>(null);
   const isInitial = useRef<boolean>(true);
-  const [currentYear, setCurrentYear] = useState<number>(value.getFullYear());
 
-  const setDate = useDateStore(state => state.setDate);
+  const { date, setDate } = useDateStore(
+    useShallow(state => ({
+      date: state.date,
+      setDate: state.setDate,
+    }))
+  );
 
   const dateList = useMemo(
     () =>
@@ -57,10 +70,23 @@ export default function MonthPicker({ startYear, endYear, value, onChangeType, o
 
   const handleViewableItemChanged = useCallback(
     ({ viewableItems }: { viewableItems: ViewToken[]; changed: ViewToken[] }) => {
+      if (viewableItems.length <= 0) {
+        return;
+      }
+
       const centerItem = viewableItems[Math.floor(viewableItems.length / 2)].item;
-      setCurrentYear(centerItem.year);
+
+      if (centerItem.year === currentDate.getFullYear()) {
+        return;
+      }
+
+      onChangeCurrentDate(prev => {
+        const newDate = new Date(prev);
+        newDate.setFullYear(centerItem.year);
+        return newDate;
+      });
     },
-    []
+    [onChangeCurrentDate, currentDate]
   );
 
   const handlePressMonth = useCallback(
@@ -77,20 +103,20 @@ export default function MonthPicker({ startYear, endYear, value, onChangeType, o
   );
 
   useEffect(() => {
-    if (!isInitial.current) {
+    if (!isInitial.current || startYear > date.getFullYear() || endYear < date.getFullYear()) {
       return;
     }
     requestAnimationFrame(() => {
-      listRef.current?.scrollToIndex({ animated: false, index: (value.getFullYear() - startYear) * 3 - 0.5 });
+      listRef.current?.scrollToIndex({ animated: false, index: (date.getFullYear() - startYear) * 3 - 0.5 });
       isInitial.current = false;
     });
-  }, [value, startYear]);
+  }, [date, startYear, endYear]);
 
   return (
     <View className="flex size-full flex-col">
       <Pressable onPress={() => onChangeType('year')}>
         <View className="flex h-12 w-full items-center justify-center rounded-lg bg-white active:bg-gray-100">
-          <Text className="text-xl font-bold">{currentYear}</Text>
+          <Text className="text-xl font-bold">{currentDate.getFullYear()}</Text>
         </View>
       </Pressable>
 
@@ -100,7 +126,7 @@ export default function MonthPicker({ startYear, endYear, value, onChangeType, o
           data={dateList}
           keyExtractor={item => `${item.year}-${String(item.monthSection)}`}
           renderItem={({ item }) => (
-            <MonthSectionRow {...item} value={value} currentYear={currentYear} onPressMonth={handlePressMonth} />
+            <MonthSectionRow {...item} currentYear={currentDate.getFullYear()} onPressMonth={handlePressMonth} />
           )}
           getItemLayout={(_, index) => ({
             length: 50,
